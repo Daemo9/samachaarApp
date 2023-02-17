707c49cc47c9397ca873ea3c557f67a7cacc769e
Bhanu <bhnprtp795@gmail.com>
2023-02-17 23:00:09 +0530

    added new branch :: develop

---
+++ src/components/NewsDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsDetails extends Component {
+  render() {
+    return (
+      <div>
+        This is news details component
+      </div>
+    )
+  }
+}
+
+export default NewsDetails
