10789512cd6f3c98e9daa7d1c248f5fe67c6365a
Bhanu <bhnprtp795@gmail.com>
2023-02-19 23:20:04 +0530

    added news app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 export default class Navbar extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
 
   render() {
     return (
