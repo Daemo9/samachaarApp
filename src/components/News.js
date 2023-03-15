03f266310efcacc0e7e5eb185f291865607dfbb5
Bhanu <bhnprtp795@gmail.com>
2023-03-15 22:25:51 +0530

    added infinite scrollbar

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import Loader from "./Loader";
 import NewsItem from "./NewsItem";
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 
 export class News extends Component {
@@ -16,22 +18,27 @@ export class News extends Component {
     pageSize: PropTypes.number,
     category: PropTypes.string,
   }
-  constructor() {
-    super();
+
+  capitalizeFirstLetter = (word)=>{
+    return word[0].toUpperCase() + word.substr(1);
+  }
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
+      totalResults: 0
     };
+    document.title =`Samachaar - ${this.capitalizeFirstLetter(this.props.category)}`;
   }
 
-  async updateNews(pageNo) {
+  async updateNews() {
     const url =
       `https://newsapi.org/v2/top-headlines?apiKey=2f2e058ecaa64fdd81c83247ff369134&category=${this.props.category}&country=${this.props.country}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData);
     this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false });
   }
   async componentDidMount() {
@@ -53,14 +60,39 @@ export class News extends Component {
   handleNextClick = async () => {
     this.setState({ page: this.state.page + 1 });
     this.updateNews();
+
+    let buttons = document.querySelectorAll('[aria-label^="Accept "]','[aria-label$="invitation"]')
+    console.log("size of list : "+buttons);
   }
+
+  fetchMoreData = async () => {
+    this.setState({
+      page: this.state.page + 1
+    });
+    const url =
+      `https://newsapi.org/v2/top-headlines?apiKey=2f2e058ecaa64fdd81c83247ff369134&category=${this.props.category}&country=${this.props.country}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: this.state.articles.concat(parsedData.articles), totalResults: parsedData.totalResults, loading: false });
+  };
+
+
   render() {
     return (
-      <div className="container my-3">
-        <h1 className="text-center" style={{ margin: '35px 0px' }}>Samachaar App : Top Headlines</h1>
-        {this.state.loading && <Loader />}
+      <>
+        <h1 className="text-center" style={{ margin: '35px 0px' }}>Samachaar App : Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
+        {/* {this.state.loading && <Loader />} */}
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.totalResults!==this.state.totalResults}
+          loader={ <Loader />}
+        >
+          <div className="container">
         <div className="row">
-          {!this.state.loading && this.state.articles.map((elem) => {
+          { this.state.articles.map((elem) => {
             return (
               <div className="col-md-4" key={elem.url}>
                 <NewsItem
@@ -78,11 +110,13 @@ export class News extends Component {
             );
           })}
         </div>
-        <div className="container d-flex justify-content-between">
+        </div>
+        </InfiniteScroll>
+        {/* <div className="container d-flex justify-content-between">
           <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous</button>
           <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>
-      </div>
+        </div> */}
+      </>
     );
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-          <img src={imgUrl} className="card-img-top" alt="..." />
+          <img src={imgUrl?imgUrl:"https://images.unsplash.com/photo-1586339949216-35c2747cc36d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}... <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ zIndex: '1', left: '90%' }}>
               {source}
